d50ca7ee10609b5437743293d4adb6058359afb0
ggpereira <ggomesper@gmail.com>
2020-05-18 21:51:21 -0300

    temperatura e mensagem

--- src/telas/menuPrincipal.js
+++ src/telas/menuPrincipal.js
@@ -1,34 +1,54 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, ImageBackground, TouchableOpacity, Text} from 'react-native'
+import { View, StyleSheet, ImageBackground, TouchableOpacity, Text } from 'react-native'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCloud, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import AsyncStorage from '@react-native-community/async-storage';
 
 import looks from '../assets/images/looks.jpeg'
-import calendar from '../assets/images/calendar.jpeg' 
+import calendar from '../assets/images/calendar.jpeg'
 import comporlook from '../assets/images/comporlook.jpeg'
 
 export default class MainMenu extends Component {
 
-    async UNSAFE_componentWillMount(){
-        
-        const token = await AsyncStorage.getItem('@Baloo:token');
-        console.log("TOKEN MAIN MENU: " + token)
-    
-        if (token === null) 
-        this.props.navigation.navigate('Login')
+    async UNSAFE_componentWillMount() {
+
+        // const token = await AsyncStorage.getItem('@Baloo:token');
+        // console.log("TOKEN MAIN MENU: " + token)
+
+        // if (token === null) 
+        // this.props.navigation.navigate('Login')
 
     }
 
     render() {
-        return(
+        return (
             <View style={styles.flexContainer}>
                 <View style={styles.flexStart}>
 
                     <View style={styles.element}>
+                        <Text style={styles.textTodayElement} > HOJE </Text>
+                    </View>
 
+                    <View style={styles.element2}>
+                        <Text style={styles.temperatureTextElement1}>10&deg;</Text>
+                        <Text style={styles.temperatureTextElement2}> temperatura </Text>
+                        <Text style={styles.temperatureTextElement3}>máx. 18&deg;</Text>
+                    </View>
+
+                    <View style={styles.element3}>
+                        <FontAwesomeIcon icon={faCloud} size={50} color={"#1B807E"} />
+                    </View>
+
+                    <View style={styles.element4}>
+                        <Text style={styles.textTomorrowElement}> AMANHÃ </Text>
+                    </View>
+
+                    <View style={styles.element5}>
+                        <FontAwesomeIcon icon={faAngleRight} color={"#E7A399"} size={50} />
                     </View>
                 </View>
 
-                <View style={styles.flexSecond}><Text>a </Text></View>
+                <View style={styles.flexSecond}><Text style={{ alignSelf: "center", paddingVertical: 8, color: "#F2F2F2" }}>Dia nublado, que tal ser um ponto de cor? </Text></View>
 
                 <View style={styles.flexThird}>
                     <View style={styles.containerImages}>
@@ -52,11 +72,11 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         justifyContent: "center",
-        alignItems: "center", 
-        backgroundColor: "#C4D0D0",
+        alignItems: "center",
+        backgroundColor: "#C4D0D0",
     },
     button1: {
-        borderRadius: 20, 
+        borderRadius: 20,
         flex: 1,
         width: 390,
         margin: 3,
@@ -88,7 +108,6 @@ const styles = StyleSheet.create({
         height: "15%",
         display: "flex",
         flexDirection: "row",
-        paddingTop: 30
     },
     flexSecond: {
         backgroundColor: "#d09f9b",
@@ -115,4 +134,51 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 10
     },
+    element: {
+        paddingLeft: 10,
+        paddingTop: 40
+    },
+
+    textTodayElement: {
+        color: "#4E3D42",
+        fontSize: 18,
+    },
+
+    element2: {
+        paddingTop: 15,
+        paddingLeft: 15
+    },
+
+    temperatureTextElement1: {
+        fontSize: 30,
+        fontWeight: "bold",
+        textAlign: "center",
+        color: "#4E3D42",
+    },
+
+    temperatureTextElement2: {
+        color: "#1B807E",
+        textAlign: "center"
+    },
+
+    temperatureTextElement3: {
+        color: "#E7A399",
+        alignSelf: "center"
+    },
+
+    element3: {
+        paddingTop: 15,
+    },
+    element4: {
+        paddingTop: 40,
+        paddingLeft: 45
+    },
+    textTomorrowElement: {
+        alignSelf: "center",
+        fontSize: 18,
+        color: "#E7A399"
+    },
+    element5: {
+        paddingTop: 30
+    }
 })
